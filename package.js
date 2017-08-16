9c3c8d8c5b630bdb54b75ad6b84ad69032886c3b
Daniel Pesch <service@danielpesch.com>
2017-08-16 11:48:05 +0200

    Shorten summary and change user handle

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  name: 'laymi:permissions',
+  name: 'peschonaut:permissions',
   version: '0.0.1',
-  summary: 'The idea behind this package is to ensure a consistent, stress-free, centralized and transparent management of permissions within a Meteor project.',
-  git: 'https://github.com/Laymi/permissions',
+  summary: 'Stress-free, centralized, and transparent management of permissions within a Meteor project.',
+  git: 'https://github.com/Peschonaut/permissions',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.2.3');
+  api.versionsFrom('1.5.1');
   api.use('ecmascript');
   api.addFiles('object-observe.js');
   api.mainModule('permissions.js');
